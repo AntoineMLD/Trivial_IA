ab329f46b29306c701efed30571ffee5167c236d
GMartin-Data <gregory.martin.data@gmail.com>
2023-12-19 11:13:14 +0100

    Implemented calculate_all_pos function in board_coordinates

--- board_coordinates.py
+++ board_coordinates.py
@@ -6,7 +6,7 @@ from config import BOARD_RADIUS, CENTER_X, CENTER_Y
 
 def calculate_pos_contour(num_points: int) -> List[int]:
     """
-    Utility function to calculate the positions
+    Helper function to calculate the box positions
     on the board's contour.
     """
     positions = []
@@ -22,25 +22,31 @@ def calculate_pos_contour(num_points: int) -> List[int]:
     return positions
 
 
-# First, evaluate positions on contour (42 boxes)
-box_positions = calculate_pos_contour(42)
+def calculate_all_pos() -> List[int]:
+    """
+    Utility function to calculate all boxes positions on the whole board. 
+    """
+    # First, evaluate positions on contour (42 boxes)
+    box_positions = calculate_pos_contour(42)
 
-# Adding box positions on "bridges",
-# diameters excluding extremities (11 points)
-for n_bridge in range(3):
-    # Defining extremities
-    begin_pos = box_positions[n_bridge * 7]
-    end_pos = box_positions[n_bridge * 7 + 21]
-    # Managing the 11 boxes on the current bridge
-    for n_box in range(1, 12):
-        fraction = n_box / 12
-        box_x = begin_pos[0] + fraction * (end_pos[0] - begin_pos[0])
-        box_y = begin_pos[1] + fraction * (end_pos[1] - begin_pos[1])
-        # Appending position if not on the center
-        if n_box != 6:
-            box_positions.append((box_x, box_y))
-        else:
-            center_position = (box_x, box_y)  # Storing the center's position for later
+    # Adding box positions on "bridges",
+    # diameters excluding extremities (11 points)
+    for n_bridge in range(3):
+        # Defining extremities
+        begin_pos = box_positions[n_bridge * 7]
+        end_pos = box_positions[n_bridge * 7 + 21]
+        # Managing the 11 boxes on the current bridge
+        for n_box in range(1, 12):
+            fraction = n_box / 12
+            box_x = begin_pos[0] + fraction * (end_pos[0] - begin_pos[0])
+            box_y = begin_pos[1] + fraction * (end_pos[1] - begin_pos[1])
+            # Appending position if not on the center
+            if n_box != 6:
+                box_positions.append((box_x, box_y))
+            else:
+                center_position = (box_x, box_y)  # Storing the center's position for later
 
-# Finally, appending the center's position
-box_positions.append(center_position)
+    # Finally, appending the center's position
+    box_positions.append(center_position)
+
+    return box_positions

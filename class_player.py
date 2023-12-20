5421c9a29c0d73d02c848bb96097f2701dc85504
AntoineMLD <antoine.moulard@hotmail.com>
2023-12-20 12:13:45 +0100

    modif question, player

--- class_player.py
+++ class_player.py
@@ -2,14 +2,14 @@ import random
 from class_question import Question
 from board_network import *
 from utils import THEMES
+
+
 class Player:
     def __init__(self, name):
         self.name = name
         self.score = {theme: False for theme in THEMES}
         self.position = central_node
-        self.correct_answers_by_theme = {}  # Dictionnaire pour stocker le score par thème
-
-
+   
     def score_point(self, category: str):
         self.score[category] = True
     
@@ -20,71 +20,15 @@ class Player:
     
     def lancer_de(self):
         result = random.randint(1, 6)
+        return result 
+
 
     def give_new_positions(self):
         result = self.lancer_de()
-        
         travel_possible = nx.shortest_path_length(G, self.position)
-        
         path_possible = []
-        
         for key,value in travel_possible.items():
             if value == result:
                 path_possible.append(key)
         return path_possible
-            
-
-    def poser_question(self, gestion_bdd):
-        themes_disponibles = [
-            "Recherche d'emploi et recrutement",
-            "Ligne de commande Terminal Linux",
-            "Git_Github",
-            "Python",
-            "Actualité IA",
-            "SQL"
-        ]
-
-    # Afficher les thèmes disponibles
-        print("Sélectionnez un thème en entrant le numéro correspondant :")
-        for i, theme in enumerate(themes_disponibles, 1):
-            print(f"{i}. {theme}")
-
-    # Demander à l'utilisateur de choisir un thème
-        choix_theme = int(input("Votre choix : "))
-
-    # Vérifier si le choix est valide
-        if 1 <= choix_theme <= len(themes_disponibles):
-            theme_selectionne = themes_disponibles[choix_theme - 1]
-            question = gestion_bdd.obtenir_question(theme_selectionne)
-        
-    
-
-        if question:
-            print(f"\nBienvenue, {self.name}!")
-            question.afficher_question()
-
-            choix = int(input("Entrez le numéro de votre réponse (1, 2, 3 ou 4): "))
-
-            if question.verifier_reponse(choix):
-                print("Bonne réponse!")
-            
-            else:
-                print("Mauvaise réponse.")
-        else:
-            print("Erreur: Aucune question trouvée.")
-
-
-    
-
-joueur1 = Player()
-
-joueur1.lancer_de()
-
-
-
-
-
-
-
-
-
+                        
--- class_question.py
+++ class_question.py
@@ -6,14 +6,8 @@ class Question:
         self.options = [option1, option2, option3, option4]
         self.answer = answer
 
-    def afficher_question(self):
-        print(f"Thème: {self.theme}")
-        print(f"Question: {self.question}")
-
-        for i, option in enumerate(self.options, start=1):
-            print(f"{i}. {option}") 
-       
-
-    def verifier_reponse(self, choix):
+    
+    def verifier_reponse(self, reponse):
         
-        return self.options[choix - 1] == self.answer
+        return self.answer == reponse
+    

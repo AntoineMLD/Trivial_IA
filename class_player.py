7223b77ec40b4f3c8cd22c7e4e2a6f30dad27da9
AntoineMLD <antoine.moulard@hotmail.com>
2023-12-20 10:08:05 +0100

    modif dans classe player pour le score

--- class_player.py
+++ class_player.py
@@ -1,29 +1,37 @@
 import random
 from class_question import Question
-
+from board_network import *
+from utils import THEMES
 class Player:
-    def __init__(self):
-        self.name = ""
-        self.score = 0
-        self.position = 0
+    def __init__(self, name):
+        self.name = name
+        self.score = {theme: False for theme in THEMES}
+        self.position = central_node
         self.correct_answers_by_theme = {}  # Dictionnaire pour stocker le score par thème
 
-    def set_name(self):
-        self.name = input("Entrez votre nom de joueur : ")
-        
-        
 
+    def score_point(self, category: str):
+        self.score[category] = True
+    
+
+    def check_victory(self):
+            return sum(self.score.values()) == 6
+        
     
     def lancer_de(self):
         result = random.randint(1, 6)
 
-        direction = input(f"{self.name}, voulez-vous vous déplacer vers la gauche (g) ou vers la droite (d) ? ").lower()
-        if direction == 'g':
-            self.position -= result
-        elif direction == 'd':
-            self.position += result
-        else:
-            print("Choix invalide. Veuillez taper g ou d.")
+    def give_new_positions(self):
+        result = self.lancer_de()
+        
+        travel_possible = nx.shortest_path_length(G, self.position)
+        
+        path_possible = []
+        
+        for key,value in travel_possible.items():
+            if value == result:
+                path_possible.append(key)
+        return path_possible
             
 
     def poser_question(self, gestion_bdd):

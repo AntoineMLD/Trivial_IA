4eef7947100673309964a9a48025c53604379a08
GMartin-Data <gregory.martin.data@gmail.com>
2023-12-20 15:30:21 +0100

    Le board apparait enfin

--- run.py
+++ run.py
@@ -1,21 +1,31 @@
 import pygame
 
-from brouillon import init_game
-from ui_functions import initialize_display, get_player_names
+from game_functions import init_game
+from ui_functions import initialize_display, get_player_names, display_board
 
 # Initialize PyGame and set it
 pygame.init()
+
 screen, font = initialize_display()
-print(type(screen))
-print(type(font))
 
 # Get players' names
 player_names = get_player_names(screen, font)
 print("Player Names:", player_names)
 
-# Initialize game
+# Initialize game components
 winner, network, positions, bdd, players = init_game(player_names)
-print("Done")
 
+# Main game loop
+running = True
+while running:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    display_board(screen, network, positions)
+
+    pygame.display.flip()
+    print("Done")
 
 pygame.quit()

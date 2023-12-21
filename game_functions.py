2ab7e8a5391e153349152b453058ac53c98ae8c9
AntoineMLD <antoine.moulard@hotmail.com>
2023-12-21 10:15:43 +0100

    update_game_after_dice

--- game_functions.py
+++ game_functions.py
@@ -2,11 +2,13 @@ from collections import deque
 from random import shuffle
 from typing import List
 from class_player import Player
-
+from board import BOARD
+from config import INDEX_TO_BOX_TYPE
 from board_network import create_game_network
 from board_coordinates import calculate_all_pos
 from class_player import Player
 from class_gestion_bdd import GestionBDD
+from typing import Tuple
 
 
 def init_game(player_names: List[str]):
@@ -17,7 +19,7 @@ def init_game(player_names: List[str]):
     - positions: List[Tuple[int, int]]
     - bdd: GestionBDD
     - players: deque d'instances de Joueur
-    - game_state : init
+    
     """
     winner = None
 
@@ -32,13 +34,24 @@ def init_game(player_names: List[str]):
     players = [Player(name) for name in player_names]
     shuffle(players)
     players = deque(players)
+        
+    return( winner, network, positions, bdd, players)
+
+def update_game_after_dice(player: Player) -> Tuple[str, bool]:
+    """
+    returns a tuple with:
+    - next action,
+    - scoring or not
+    - theme
+    """
+    scoring = False
+    box = INDEX_TO_BOX_TYPE[player.position]
+ 
+    if box == "Again":
+        return box, scoring
+
+    if box.startswith("*"):
+        scoring = True
+        box = box[1:]
     
-    # Init game state to A 
-    game_state = "A"
-
-    return winner, network, positions, bdd, players, game_state
-
-
-def again_game_state():
-    # si sur une case "again" alors obtient le statut "A"
-    # 
+    return box, scoring

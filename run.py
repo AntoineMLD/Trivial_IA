74266a7f54d5d57c54a51a6106b0f16afa3128d5
AntoineMLD <antoine.moulard@hotmail.com>
2023-12-22 01:48:21 +0100

    espace en trop supprimé

--- run.py
+++ run.py
@@ -14,7 +14,6 @@ G = create_game_network()
 screen, board_zone, info_zone, score_zone, question_zone, font = initialize_display()
 
 input_box = pygame.Rect(600, 50, 200, 32)
-
 dice_box = pygame.Rect(900, 10, 50, 50)  # Positioned in the right zone
 max_players = 1
 active = False
@@ -25,13 +24,14 @@ start_game = False
 dice_result = '6'
 available_cases = []
 positions_chosen = []
-
 BDD = GestionBDD('questions.db')
+
 # Définir les coordonnées de chaque réponse
 response_1 = (495, 541)
 response_2 = (541, 541)
 response_3 = (495, 577)
 response_4 = (541, 577)
+
 # Create text surfaces for the numbers
 font_size = 25  # Adjust the font size as needed
 font_numbers = pygame.font.SysFont(None, font_size)
@@ -39,6 +39,7 @@ text_1 = font_numbers.render(" 1", True, RED)
 text_2 = font_numbers.render(" 2", True, RED)
 text_3 = font_numbers.render(" 3", True, RED)
 text_4 = font_numbers.render(" 4", True, RED)
+
 # Créer des rectangles à partir de ces coordonnées
 width = 20  # Choisir la largeur appropriée pour les zones de réponse
 height = 20  # Choisir la hauteur appropriée pour les zones de réponse
@@ -52,10 +53,8 @@ selected_answer = None
 question_answered = False  # Variable de contrôle
 
 
-
 while True:
-    
-
+  
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
@@ -74,23 +73,23 @@ while True:
 
             # Vérification de la réponse sélectionnée
             if selected_answer is not None and not question_answered:
-                # Obtenez la réponse correspondant au chiffre sélectionné depuis la question
+                # Obtenir la réponse correspondant au chiffre sélectionné depuis la question
                 selected_option = question.options[selected_answer - 1]
 
-                # Vérifiez si la réponse sélectionnée correspond à la réponse correcte
+                # Vérifie si la réponse sélectionnée correspond à la réponse correcte
                 if selected_option == question.answer:
                     # Si la réponse est correcte
                     print("Bonne réponse !")
-                    player.score_point(question.theme)  # Marquer un point pour la question
+                    player.score_point(question.theme)  # Marque un point pour la question
                     question_answered = True
-                    # Mettez ici votre logique de scoring pour une bonne réponse
-
-                    # Afficher "Tu as gagné ! Relance le dé" après avoir gagné
+                    
+                    # Affiche "Tu as gagné ! Relance le dé" après avoir gagné
                     winning_text = font.render("Tu as gagné ! Relance le dé", True, WHITE)
-                    screen.blit(winning_text, (question_zone.x, question_zone.y))  # Afficher le message de victoire
-                    pygame.display.flip()  # Mettre à jour l'écran pour afficher le message
+                    screen.blit(winning_text, (question_zone.x, question_zone.y))  # Affiche le message de victoire
+                    pygame.display.flip()  # Met à jour l'écran pour afficher le message
                     last_question = question
-                    # Masquer la question après avoir affiché le message de victoire
+                    
+                    # Masque la question après avoir affiché le message de victoire
                     display_question_and_handle_answer(screen, font, None, display_question=False, display_winning_text=True)
                     selected_answer = None 
                     question = None
@@ -116,7 +115,7 @@ while True:
             # When player click on the case choosen
             else:
                 for idx, pos in enumerate(positions):
-                    # Vérifier si le clic est à l'intérieur du cercle entouré en rouge
+                    # Vérifie si le clic est à l'intérieur du cercle entouré en rouge
                     if pygame.mouse.get_pos()[0] >= pos[0] - BOX_RADIUS - 3 and pygame.mouse.get_pos()[0] <= pos[0] + BOX_RADIUS + 3:
                         if pygame.mouse.get_pos()[1] >= pos[1] - BOX_RADIUS - 3 and pygame.mouse.get_pos()[1] <= pos[1] + BOX_RADIUS + 3:
                             if idx in available_cases:
@@ -126,21 +125,19 @@ while True:
                                 print("Positions choisies :", positions_chosen)
                                 player.position = positions_chosen[-1]
                                 print(f"Position du {player.name} : {player.position}")
-                                
-                               
                             
-                            # Effacer tout le plateau
+                            # Efface tout le plateau
                             screen.fill(BACKGROUND)
 
-                            # Redessiner chaque case avec sa couleur d'origine
+                            # Redessine chaque case avec sa couleur d'origine
                             for idx, pos in enumerate(positions):
                                 box_color = BOX_TYPE_TO_COLOR[network.nodes[idx]['box_type']]
                                 pygame.draw.circle(screen, box_color, pos, BOX_RADIUS)
 
-                            # Dessiner le cercle du joueur dans sa nouvelle position
+                            # Dessine le cercle du joueur dans sa nouvelle position
                             pygame.draw.circle(screen, RING_COLOR, positions[player.position], BOX_RADIUS + RING_WIDTH, RING_WIDTH)
 
-                            # Mettre à jour l'écran
+                            # Met à jour l'écran
                             pygame.display.flip()
                             
                             theme, scoring = check_current_case(player)
@@ -149,19 +146,13 @@ while True:
                                 roll_dice_message = font.render("Relance le dé", True, WHITE)
                                 screen.blit(roll_dice_message, (question_zone.x, question_zone.y))  # Position à ajuster
                                 pygame.display.flip()
-
-                                
+  
                             if theme != "Again":
                                 question = BDD.obtenir_question(theme)
                                 display_question_and_handle_answer(screen, font, question)
                                 last_question = None
                             question_answered = False
-                                 
-                            
-                                        
-                            
-                            
-       
+        
         elif event.type == pygame.KEYDOWN:
             if active:
                 if event.key == pygame.K_RETURN:
@@ -191,9 +182,7 @@ while True:
         
         # Displaying scores
         display_scores(screen, font, player)
-            
-
-
+         
     # Clear the input area (600px wide, 100px high)
     pygame.draw.rect(screen, BACKGROUND, (600, 0, 600, 100))
     
